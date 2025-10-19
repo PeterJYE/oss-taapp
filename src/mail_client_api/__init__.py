ec576e91a524ca56d0b05c493373c2a9076fe6ec
rachitt <100853011+rachitt@users.noreply.github.com>
2025-10-19 17:23:07 -0400

    Update __init__.py

--- src/mail_client_api/__init__.py
+++ src/mail_client_api/__init__.py
@@ -1,5 +1,5 @@
-from .src.mail_client_api import Client, get_client, Message, get_message
+from .src.mail_client_api.client import Client, get_client
+from .src.mail_client_api.message import Message, get_message
 
 __all__ = ["Client", "get_client", "Message", "get_message"]
-
 
